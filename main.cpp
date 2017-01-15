422d3112ca338408a0a5e7f622e9a066fedbd2e8
agauniyal <agauniyal@outlook.com>
2017-01-15 13:20:07 +0530

    Add error handling example for capablities

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "termdb.hpp"
+#include "include/termdb.hpp"
 #include <iostream>
 
 int main()
@@ -7,16 +7,17 @@ int main()
 	auto result = parser.parse("xterm");
 
 	if (result) {
-		auto name = parser.getTermName();
-		auto b    = parser.getCapBin(cap::bin::has_meta_key);
-		auto n    = parser.getCapNum(cap::num::columns);
-		auto s    = parser.getCapStr(cap::str::enter_bold_mode);
+		auto name    = parser.getTermName();
+		auto boolean = parser.getCapBin(cap::bin::has_meta_key);
+		auto numeric = parser.getCapNum(cap::num::columns);
+		auto str     = parser.getCapStr(cap::str::enter_bold_mode);
 
 		std::cout << "\n"
 		          << "Name: " << name << "\n"
-		          << "Has Meta Key: " << std::boolalpha << b << "\n"
-		          << "Number of columns: " << n << "\n"
-		          << "Bold Mode on: " << s << "I should be BOLD!"
+		          << "Has Meta Key: " << std::boolalpha << boolean << "\n"
+		          << "Number of columns(-1 if not present): "
+		          << ((numeric != cap::NP) ? numeric : -1) << "\n"
+		          << "Bold Mode on: " << str << "I should be BOLD!"
 		          << "\n";
 	} else {
 		std::cerr << "Couldn't parse terminfo database\n";
