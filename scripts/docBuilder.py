6dc4c88fde12d695fb97de93997196c3d2df87c7
tushar khurana <tusharkhurana99@gmail.com>
2018-02-20 23:16:40 +0530

    Fix the docBuilder script to only append once to test/meson.build

--- scripts/docBuilder.py
+++ scripts/docBuilder.py
@@ -19,16 +19,26 @@ if __name__ == "__main__":
     mesonFile = outDir.joinpath("meson.build")
     lineToAppend = '''{name} = executable('{name}', '{filename}', include_directories : inc, dependencies : [optional, variant])\n'''
 
-    with open(str(mesonFile), 'a') as meson:
+    with open(str(mesonFile), 'a+') as meson:
+        meson.seek(0)
+        mesonContent = meson.read().splitlines(True)
+
         for eachMdFile in mdFiles:
             with open(str(eachMdFile)) as f:
                 content = f.read()
-                codeSegments =[x.split('```')[0] for x in content.split('```cpp') if '```' in x]
-                
+                codeSegments = [x.split('```')[0]
+                                for x in content.split('```cpp') if '```' in x]
+
                 for idx, eachSegment in enumerate(codeSegments):
                     filename = str(nameFormat).format(number=idx+1)
-                    with open(filename, 'w') as f2:
-                        f2.write(eachSegment)
                     x = basename(filename)
-                    meson.write(lineToAppend.format(filename=x, name=x.split('.')[0]))
+
+                    line = lineToAppend.format(
+                        filename=x, name=x.split('.')[0])
+
+                    if line not in mesonContent:
+                        with open(filename, 'w') as f2:
+                            f2.write(eachSegment)
+                        meson.write(line)
+
     print("[docBuilder] Docs Building Completed...")
